62562d7535837324a53a2a36fdaca5ea793f3b09
Ari Petäjäjärvi <fpvonly@gmail.com>
2020-10-01 18:41:06 +0300

    Initial project work

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var nocache = require('node-nocache');
+
+var app = express();
+var server = app.listen(40, function() {
+  console.log("Picmapper server started");
+});
+
+app.use(nocache); // prevent client caching for images, sounds etc
+app.use('/', express.static(path.resolve('')));
+app.use('/assets', express.static(path.resolve('assets')));
+app.use('/build', express.static(path.resolve('build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(  __dirname + '/index.html' );
+});
